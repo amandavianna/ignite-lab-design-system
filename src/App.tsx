50bff4d6c98d6e28002216df65ec1fea5747ff4c
Amanda Vianna <amandavianna@MacBook-Pro-de-Amanda.local>
2022-10-13 09:20:59 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold text-2xl text-gray-200'>Hello World</h1>
+
+      <button className='font-medium px-4 py-2 rounded text-white hover:bg-cyan-300 bg-cyan-500'>
+        Enviar
+      </button>
+    </>
+  )
+}
